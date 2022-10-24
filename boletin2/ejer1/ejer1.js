7842d0f178c436033255bb28412d2cde3f90e43b
ClaraIglesias18 <claraiglesias18@hotmail.com>
2022-10-24 18:45:22 +0200

    24/10

--- boletin2/ejer1/ejer1.js
+++ boletin2/ejer1/ejer1.js
@@ -1,10 +1,8 @@
 var nombres = new Array("España", "Francia", "Suecia", "Italia", "Noruega", "Portugal", "Holanda");
 var salida = "";
 
-//eliminar "España" e introducir en su lugar "Alemania", "Dinamarca", "Grecia", "Suiza"
 nombres.splice(0, 1, "Alemania", "Dinamarca", "Grecia", "Suiza");
 nombres.sort();
-//insertar por el principio
 nombres.unshift("Belgica");
 
 for(clave in nombres) {
--- boletin2/ejer3/ejer3.js
+++ boletin2/ejer3/ejer3.js
@@ -1,5 +1,27 @@
-var nombres = new Array("Jose", "Julio", "Luis", "Bea", "Julio", "Maria", "Luis", "Luis", "Raul", "Jose Luis");
-var modulos = new Array("Interfaces", "Despliegue", "Servidor", "Cliente", "Sistemas", "Redes", "BaseDatos", "Lima", "Implantacion", "Seguridad");
+var nombres = new Array(
+    "Jose",
+    "Julio",
+    "Luis",
+    "Bea",
+    "Julio",
+    "Maria",
+    "Luis",
+    "Luis",
+    "Raul",
+    "Jose Luis"
+);
+var modulos = new Array(
+    "Interfaces",
+    "Despliegue",
+    "Servidor",
+    "Cliente",
+    "Sistemas",
+    "Redes",
+    "BaseDatos",
+    "Lima",
+    "Implantacion",
+    "Seguridad"
+);
 var alumnos = new Array(21, 27, 30, 15, 10, 20, 25, 32, 13, 16);
 var nombre = prompt("Dime el nombre del profesor: ");
 var imparte = false;
@@ -11,50 +33,63 @@ var mayorNumAlum = 0;
 var posMayorNumAlum;
 var salida = "";
 
-if(Number(nombre)) {
+if (Number(nombre)) {
     salida += "Debes introducir un nombre no un numero";
-} else if(!nombre) {
+} else if (!nombre) {
     salida += "No puedes dejar la entrada vacia";
 } else {
-
-    for(let i = 0; i <= nombres.length - 1; i++) {
-        if(nombre == nombres[i]) {
-            imparte = true;
-        }
+    let i = 0;
+    while (nombres[i] != nombre && i < nombres.length - 1) {
+        i++;
     }
 
-    if(imparte) {
-        for(let i = 0; i <= nombres.length - 1; i++){
-            if(nombres[i] == nombre){
+    if (nombres[i] == nombre) {
+        countMod++;
+        modulosImparte += modulos[i] + " ";
+        sumaAlumnos += alumnos[i];
+
+        for (let k = i + 1; k <= nombres.length - 1; k++) {
+            if (nombres[k] == nombre) {
                 countMod++;
-                modulosImparte += modulos[i] + " ";
-                sumaAlumnos += alumnos[i];
-            }  
+                modulosImparte += modulos[k] + " ";
+                sumaAlumnos += alumnos[k];
+            }
         }
-    
-        salida += "El profesor " + nombre + " imparte algun modulo."
-        + "\nEl profesor imparte " + countMod + " modulo/s."
-        + "\nImparte: " + modulosImparte
-        + "\nTiene un total de: " + sumaAlumnos + " alumnos."
-        + "\nUna media de " + sumaAlumnos/countMod + " alumnos por modulo.";
-    
+
+        salida +=
+            "El profesor " +
+            nombre +
+            " imparte algun modulo." +
+            "\nEl profesor imparte " +
+            countMod +
+            " modulo/s." +
+            "\nImparte: " +
+            modulosImparte +
+            "\nTiene un total de: " +
+            sumaAlumnos +
+            " alumnos." +
+            "\nUna media de " +
+            sumaAlumnos / countMod +
+            " alumnos por modulo.";
     } else {
         salida += nombre + " no es ningun profesor";
     }
 }
 
-for(let i = 0; i <= alumnos.length - 1; i++) {
-    if(alumnos[i] > mayorNumAlum) {
+for (let i = 0; i <= alumnos.length - 1; i++) {
+    if (alumnos[i] > mayorNumAlum) {
         mayorNumAlum = alumnos[i];
         posMayorNumAlum = i;
     }
 }
 
-salida += "\nEl profesor " + nombres[posMayorNumAlum]
-    + " es el que tiene mayor numero de alumnos matriculados en el modulo "
-    + modulos[posMayorNumAlum] + " con un numero de " + alumnos[posMayorNumAlum] + " alumnos.";
+salida +=
+    "\nEl profesor " +
+    nombres[posMayorNumAlum] +
+    " es el que tiene mayor numero de alumnos matriculados en el modulo " +
+    modulos[posMayorNumAlum] +
+    " con un numero de " +
+    alumnos[posMayorNumAlum] +
+    " alumnos.";
 
 alert(salida);
-
-
-
