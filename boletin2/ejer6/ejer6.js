266096f8470414a02e693ea100854acf6eb6ac63
ClaraIglesias18 <claraiglesias18@hotmail.com>
2022-11-15 19:26:02 +0100

    15/11

--- boletin2/ejer6/ejer6.js
+++ boletin2/ejer6/ejer6.js
@@ -23,7 +23,7 @@ var asignaturas = {
     102: ["ADSO", "PROG", "DWCC"],
     103: ["DIW", "SERI"],
     104: ["DWCS", "PROG", "DIW"],
-    105: ["ADSO", "DIW"], 
+    105: ["ADSO", "DIW"],
     106: ["PROG", "SERI"],
 };
 
@@ -32,15 +32,15 @@ var horario = {
     segunda: new Array("DAW", "DIW", "DWCC"),
     tercera: new Array("ADSO", "PROG", "PROG"),
     cuarta: new Array("SERI", "PROG", "DWCS"),
-    quinta:new Array("SERI", "DIW", "DWCS"),
-    sexta: new Array("EIE", "DWCC", "DWCC")
+    quinta: new Array("SERI", "DIW", "DWCS"),
+    sexta: new Array("EIE", "DWCC", "DWCC"),
 };
 var salida = "";
 var entrada = prompt("Indica el alumno del que quieres saber informacion: ");
 
 if (isNaN(entrada)) {
     salida += "Tienes que introducir un numero de alumno";
-} else if(alumno[entrada] == undefined) {
+} else if (alumno[entrada] == undefined) {
     salida += "El alummo no pertenece al centro";
 } else {
     salida += "El alumno " + alumno[entrada] + " pertenece al centro\n";
@@ -51,7 +51,6 @@ if (isNaN(entrada)) {
         salida += clave + "    | ";
 
         horario[clave].forEach((element) => {
-
             if (asignaturas[entrada].includes(element)) {
                 salida += element + "(" + modulos[element] + ")  |";
             } else {
@@ -61,41 +60,37 @@ if (isNaN(entrada)) {
         salida += "\n";
     }
 }
+  
 
 function matriculado(alumno, modulo) {
     let salida;
-    if(asignaturas[alumno].includes(modulo)) {
+    
+    if (asignaturas[alumno].includes(modulo)) {
         salida = "El alumno " + alumno + " esta matriculado en " + modulo;
     } else {
-        salida =  "El allumno " + alumno + " no esta matriculado en " + modulo;
+        salida = "El alumno " + alumno + " no esta matriculado en " + modulo;
     }
 
     return salida;
 }
 
 function diaClase(alumno, dia) {
-    let salida;
     let clase = false;
+
     for (const clave in horario) {
-        if(alumno.includes(horario[clave][dia])) {
+        if (asignaturas[alummo].includes(horario[clave][dia])) {
             clase = true;
         }
     }
 
-    if(clase) {
-        salida = "Tiene clase";
-    } else {
-        salida = "No tiene clase";
-    }
-
-    return salida;
+    return clase;
 }
 
 function numeroAlumnos(asignatura) {
     let cont = 0;
-    
+
     for (const clave in asignaturas) {
-        if(asignaturas[clave].includes(asignatura)) {
+        if (asignaturas[clave].includes(asignatura)) {
             cont++;
         }
     }
@@ -105,16 +100,25 @@ function numeroAlumnos(asignatura) {
 
 alert(salida);
 
-var alumno = prompt("Dime el alumno que quieras saber si esta matriculado: ");
-var modulo = prompt("Dime el modulo en el que quieras buscar: ");
+var alumno = prompt(
+    "---- INFORMACION SOBRE MATRICULA ----" +
+        "\nIndica el alumno que quieres saber si esta matriculado: "
+);
+var modulo = prompt("Indica el modulo en el que quieras buscar: ");
 
 alert(matriculado(alumno, modulo));
 
-var alummo = prompt("dime el alumno que quieras saber informacion: ");
+var alummo = prompt(
+    "---- INFORMACION SOBRE CLASES ------" +
+        "\nIndica el alumno que quieras saber informacion: "
+);
 var dia = prompt("Indica el dia: ");
 
 alert(diaClase(alummo, dia));
 
-var asignatura = prompt("Indica la asignatura para contar el numero de alumnos: ");
+var asignatura = prompt(
+    "---- NUMERO DE ALUNOS POR ASIGNATURA ----" +
+        "\nIndica la asignatura para contar el numero de alumnos: "
+);
 
 alert(numeroAlumnos(asignatura));
